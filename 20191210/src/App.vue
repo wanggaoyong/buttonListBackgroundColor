fd21e410b45f744ffb49d780c8ec7b0a909b73d2
wanggaoyong <wanggaoyong@gwssi.com.cn>
2019-12-10 14:20:26 +0800

    选项按钮增加不同的背景颜色

---
+++ 20191210/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="mian">
+    <!-- <router-view></router-view> -->
+    <ul>
+      <li v-for="(item,index) in list" :key="index" @click="fn(item,index)" 
+      :class="{active:item.donw=='down'&& flag==index,red:item.donw=='up' && flag==index}">one{{index}} </li>
+    
+    </ul>
+    <Home v-show="false"/>
+  </div>
+</template>
+
+<script>
+// import Home from './components/HelloWorld.vue'
+export default {
+  data(){
+    return{
+      flag:0,
+      list:[
+        {donw:'down',up:'up'},
+        {donw:'',up:'up'},
+        {donw:'',up:'up'},
+      ]
+    }
+  },
+  methods:{
+      fn(e,val){
+      this.flag=val;
+      e.donw=='down'?e.donw='up':e.donw='down';
+     }
+  },
+  //  
+components:{
+  Home:()=>import('./components/HelloWorld.vue')
+}
+}
+</script>
+
+<style>
+ ul>li:hover{
+        background-color:blue;
+      }
+      ul>li.red{
+        background-color:red;
+      }
+      ul>li.color{
+         background-color: yellow;
+      }
+      ul>li.active{
+        background-color:blue;
+      }
+      
+      ul>li{
+        list-style: none;
+        width: 60px;
+        height: 20px;
+        background-color:lightblue;
+        float:left;
+        text-align: center;
+        line-height: 20px;
+        cursor: pointer;
+      }
+     
+      ul>li+li{
+        margin-left: 10px;
+      }
+      
+     
+</style>
